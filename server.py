8bca9df1717f3f6cea549750d97d6695363c84ce
klestyselymay <dhecxba@gmail.com>
2022-10-22 14:08:09 +0200

    2.0-update

--- server.py
+++ server.py
@@ -4,39 +4,43 @@ import json
 import datetime
 from colorama import Fore
 import subprocess
+import time
+import re
 
 subprocess.call('cls', shell=True)
 
+RESET = '\033[0m'
+def get_color_escape(r, g, b, background=False):
+    return '\033[{};2;{};{};{}m'.format(48 if background else 38, r, g, b)
+
 SERVER_HOST = "127.0.0.1"
 SERVER_PORT = 5006
-separator_token = ": "
 
 with open('server.json') as f:
     server1 = json.load(f)
 
 with open('server.json') as ff:
     server2 = ff.read()
-ch1 = u'\u2713'
-
-if server1['colors'][0]['color'] == 'green':
-    print(Fore.GREEN)
-elif server1['colors'][0]['color'] == 'blue':
-    print(Fore.BLUE)
-elif server1['colors'][0]['color'] == 'red':
-    print(Fore.RED)
 
 client_sockets = set()
 s = socket.socket()
+print(f"{get_color_escape(21, 170, 13)}[SERVER]{Fore.WHITE} Successfully created socket")
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+time.sleep(.6)
 s.bind((SERVER_HOST, SERVER_PORT))
+print(f"{get_color_escape(21, 170, 13)}[SERVER]{Fore.WHITE} Successfully created bind")
+time.sleep(.6)
 s.listen(5)
-print(f"[*] Listening as {SERVER_HOST}:{SERVER_PORT}")
+print(f"{get_color_escape(21, 170, 13)}[SERVER]{Fore.WHITE} Listening as {SERVER_HOST}:{SERVER_PORT}")
 
 def listen_for_client(cs):
     while True:
         try:
             msg = cs.recv(1024).decode()
-            print(msg)
+            message = msg
+            message_s = re.findall(r'\[.*?\]', message)
+            message = message.replace(f'{message_s[0]} ', '')
+            print(get_color_escape(21, 170, 13)+message_s[0],Fore.WHITE,message)
             if '//' in msg:
                 cmd = msg
                 cmd = cmd.split('=')
@@ -47,7 +51,7 @@ def listen_for_client(cs):
                     cmd_out = '/'+cmd_name+'/'+cmd_out
                     cs.send(cmd_out.encode())
                 else:
-                    cs.send(f'//{cmd_name}[!] Command does not exist'.encode())
+                    cs.send(f'//{cmd_name}[SERVER] Command does not exist'.encode())
 
         except Exception as e:
             print(f"[!] Error: {e}")
@@ -60,12 +64,12 @@ def listen_for_client(cs):
 while True:
     client_socket, client_address = s.accept()
     c_name = client_socket.recv(1024).decode()
-    print(f"[+] {client_address} Connected as {c_name}")
+    print(f"{get_color_escape(21, 170, 13)}[SERVER]{Fore.WHITE} {client_address} Connected as {c_name}")
     client_sockets.add(client_socket)
     t = Thread(target=listen_for_client, args=(client_socket,))
     t.daemon = True
     t.start()
-    print(f"[{ch1}] {client_address} Verified as {c_name}")
+    print(f"{get_color_escape(21, 170, 13)}[SERVER]{Fore.WHITE} {client_address} Verified as {c_name}")
 for cs in client_sockets:
     cs.close()
 s.close()
